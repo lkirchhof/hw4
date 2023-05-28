6386e2db9541fd144261e9b437e453b7f026ba12
Luisa Kirchhof <luisa.kirchhof@kellogg.northwestern.edu>
2023-05-28 02:52:04 +0000

    created login controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    if @user = !nil
+      if @user["password"] == params["password"]
+        flash["notice"] = "Welcome"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Incorrect login information. Please try again"
+        redirect_to "/sessions/new"
+      end
+    else
+    flash["notice"] = "Incorrect login information. Please try again"
+    redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    
+    redirect_to "/sessions/new"
+
   end
 end
   
